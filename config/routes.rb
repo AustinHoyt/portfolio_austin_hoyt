61e4e683f0689dddcaa1bbe4ba3f11d81c90734c
Austin Hoyt <ahoytdeveloper@gmail.com>
2017-06-02 00:34:05 +0000

    Added an idea using parallax and animation but not the biggest fan

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    
+  root to: 'demo#index'
+  get 'about', to: 'demo#about'
+  
+  
 end
